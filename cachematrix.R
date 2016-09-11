950c49327b6be0b781e250684e519e0629806a3a
kuede <kudduesi.uenlue@gmx.de>
2016-09-11 12:30:33 +0200

    cachematrix.R init

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+####################################################################################################################
+##  
+## author: kudduesi uenlue
+## date:   09.11.2016
+##
+## 1.) makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##
+## 2.) cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##     If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+##     retrieve the inverse from the cache.
+##
+## assumption: the matrix supplied is always invertible.
+##
+####################################################################################################################
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix  <- function( myMatrix = matrix() ){
+  
+  myInverse <- NULL
+  
+  setMatrix <- function(setMatrix) {
+    myMatrix  <<- setMatrix
+    myInverse <<- NULL
+  }
+  
+  getMatrix  <- function() myMatrix
+  
+  setInverse <- function(inverse) myInverse <<- inverse
+  
+  getInverse <- function() myInverse
+  
+  list(setMatrix  = setMatrix, 
+       getMatrix  = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function( oMatrix, ... ){
+  
+  inverse <- oMatrix$getInverse()
+  
+  if(!is.null(inverse)) {
+    message("!!! inverse of this matrix was already calculated !!!")
+    return(inverse)
+  }
+  
+  calcMatrix <- oMatrix$getMatrix()
+  inverse <- solve( calcMatrix , ... )
+  oMatrix$setInverse( inverse )
+  inverse
+  
 }
